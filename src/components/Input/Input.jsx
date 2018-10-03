e0abf58535558f2639cccbc3b2ce135c402211d8
Bharat Soni <i.bharat.soni@gmail.com>
2018-10-03 15:34:48 +0800

    FIX: fix small issue

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -16,7 +16,7 @@ export default class Input extends Component {
         inputClass = '',
         errorClass = '',
         labelClass = ''
-      }
+      } = {}
     } = this.props
     const {
       setFieldValue,
