6a5c6c9e79504a797f9e701ec5adb353c4bb5ce0
Bharat Soni <i.bharat.soni@gmail.com>
2019-01-21 17:43:36 +0800

    FEAT: Add all tests for input component

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -33,14 +33,15 @@ const contextTypes = {
   validateForm  : PropTypes.func,
 }
 const defaultInputArgs = {
-  id            : 'email',
-  type          : 'text',
-  value         : '',
-  onFieldChange : null,
-  validate      : 'required',
-  placeholder   : 'enter value',
-  displayName   : 'Email',
-  classes       : {
+  id                      : 'email',
+  type                    : 'text',
+  value                   : '',
+  onFieldChange           : null,
+  validate                : 'required',
+  placeholder             : 'enter value',
+  displayName             : 'Email',
+  shouldUseDefaultClasses : true,
+  classes                 : {
     contClass  : 'cont-class',
     fieldClass : 'field-class',
     errorClass : 'error-class',
@@ -65,6 +66,23 @@ describe('>>> Input Container -- Snapshot Test', () => {
     const tree = renderer.create(
       <FormContainer>
         <Input id='email' value='test' />
+      </FormContainer>
+    ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Matches the snapshot with label in input', () => {
+    const customProps = { ...defaultInputArgs }
+    const customFormProps = { ...defaultContext.formData }
+
+    customProps.label = 'Input label'
+    customProps.placeholder = 'custom placeholder'
+    customProps.displayName = 'custom display name'
+
+    const tree = renderer.create(
+      <FormContainer defaultClasses={customProps.defaultClasses}>
+        <Input {...customProps} />
       </FormContainer>
     ).toJSON()
 
@@ -105,6 +123,53 @@ describe('>>> Input Container -- Snapshot Test', () => {
 
     expect(tree).toMatchSnapshot()
   })
+
+  it('Matches the snapshot with error in input', () => {
+    const customContext = { ...defaultContext }
+    const props = {
+      ...defaultInputArgs,
+      onFieldChange : jest.fn(),
+      validate : 'email'
+    }
+
+    customContext.formData.fields.email = props
+    customContext.formData.errors = {
+      email : 'some error'
+    }
+
+    const WrappedWithContextComponent = wrapWithContext(customContext, contextTypes)
+    const tree = renderer.create(
+      <WrappedWithContextComponent>
+        <Input { ...props } />
+      </WrappedWithContextComponent>
+    ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Matches the snapshot without using default classes provided by form', () => {
+    const customContext = { ...defaultContext }
+    const props = {
+      ...defaultInputArgs,
+      validate                : 'email',
+      onFieldChange           : jest.fn(),
+      shouldUseDefaultClasses : false
+    }
+
+    customContext.formData.fields.email = props
+    customContext.formData.errors = {
+      email : 'some error'
+    }
+
+    const WrappedWithContextComponent = wrapWithContext(customContext, contextTypes)
+    const tree = renderer.create(
+      <WrappedWithContextComponent>
+        <Input { ...props } />
+      </WrappedWithContextComponent>
+    ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
 })
 // *************************************************************
 
@@ -115,10 +180,6 @@ describe('>>> Input Container -- Snapshot Test', () => {
 describe('>>> Input Container -- Shallow Rendering', () => {
   let mountedScreen
   const inputScreen = (customProps = {}, customContext = {}) => {
-    const context = {
-      ...defaultContext,
-      ...customContext
-    }
     const initialState = {
       id    : 'email',
       value : ''
