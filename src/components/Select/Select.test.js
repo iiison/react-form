1c7dc9ac0ef3ef201cc66317a8dc9beb03e4549c
iiison <i.bharat.soni@gmail.com>
2019-10-31 18:48:04 +0800

    FEAT: Add all tests for Select Container

--- src/components/Select/Select.test.js
+++ src/components/Select/Select.test.js
@@ -48,26 +48,26 @@ const defaultArgs = {
     labelClass : 'label-class'
   },
 }
+const options = [
+  {
+    id          : 'apple',
+    value       : 'applee',
+    displayName : 'Fruit Apple'
+  },
+  {
+    id          : 'orange',
+    value       : 'orange',
+    displayName : 'Fruit orange'
+  },
+  {
+    id          : 'pineapple',
+    value       : 'pineapple',
+    displayName : 'Fruit Pineapple'
+  }
+]
 
 describe('>>> Select Container -- Snapshot Test', () => {
   it('Matches the snapshot with default props', () => {
-    const options = [
-      {
-        id          : 'apple',
-        value       : 'applee',
-        displayName : 'Fruit Apple'
-      },
-      {
-        id          : 'orange',
-        value       : 'orange',
-        displayName : 'Fruit orange'
-      },
-      {
-        id          : 'pineapple',
-        value       : 'pineapple',
-        displayName : 'Fruit Pineapple'
-      }
-    ]
     const selectPops = {
       ...defaultArgs,
       options
@@ -80,6 +80,109 @@ describe('>>> Select Container -- Snapshot Test', () => {
 
     expect(tree).toMatchSnapshot()
   })
-})
 
+  it('Matches the snapshot with custom props', () => {
+    const props = {
+      ...defaultArgs,
+      options,
+      label               : 'Select Fruits',
+      value               : 'pineapple',
+      placeholder         : 'Select One...',
+      displayName         : 'Fruits',
+      shouldValidateField : true,
+      classes             : {
+        contClass           : 'select-cont',
+        labelClass          : 'select-label',
+        fieldClass          : 'select-box',
+        errorClass          : 'select-error',
+        optionClass         : 'select-option',
+        selectedOptionClass : 'selected-value'
+      }
+    }
+
+    const tree = renderer.create(
+      <FormContainer>
+        <Select {...props} />
+      </FormContainer>
+    ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Matches the snapshot with error in select box', () => {
+    const customContext = { ...defaultContext }
+    const props = {
+      ...defaultArgs,
+      options,
+      onFieldChange : jest.fn(),
+      validate      : 'required'
+    }
+
+    customContext.formData.fields.selectBox = props
+    customContext.formData.errors = {
+      selectBox : 'some error'
+    }
+
+    const WrappedWithContextComponent = wrapWithContext(customContext, contextTypes)
+    const tree = renderer.create(
+      <WrappedWithContextComponent>
+        <Select {...props} />
+      </WrappedWithContextComponent>
+    ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
+// *************************************************************
+
+// Test DOM Behaviour
+describe('>>> Select Box Container -- Shallow Rendering', () => {
+  let mountedScreen
+  const selectScreen = (customProps = {}) => {
+    const initialState = {
+      id    : 'selectBox',
+      value : '',
+      options
+    }
+
+    if (!mountedScreen) {
+      const updatedState = {
+        ...initialState,
+        ...customProps
+      }
+
+      mountedScreen = mount(<FormContainer><Select {...updatedState} /></FormContainer>)
+    }
+
+    return mountedScreen
+  }
+
+  beforeEach(() => {
+    mountedScreen = undefined
+  })
+
+  it('Triggers the change function when select value is changed', () => {
+    const props = {
+      events : {
+        onChange : jest.fn()
+      }
+    }
+
+    const tree = selectScreen(props)
+    const optionsInTree = tree.find({role : 'button'})
+    const optionOrange = optionsInTree.at(2)
+
+    optionOrange.simulate('click')
+
+    const expectedValue = 'Fruit orange'
+    const selectedValue = tree
+      .find('.field-value')
+      .at(0)
+      .text()
+
+    expect(selectedValue).toEqual(expectedValue)
+    expect(props.events.onChange).toHaveBeenCalled()
+  })
+})
 /* eslint-enable*/
+
